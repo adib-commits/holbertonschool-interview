37363f4b98fb3913de9285e3204ba99c0cda96a2
adib-commits <hdadib442@gmail.com>
2026-05-27 14:58:31 +0200

    That calculates the fewest number of operations needed to result in exactly n H characters in the file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,19 +3,8 @@
 Module that calculates the minimum number
 of operations needed to obtain n H characters.
 """
-
-
 def minOperations(n):
-    """
-    Calculates the fewest number of operations
-    needed to result in exactly n H characters.
 
-    Args:
-        n (int): target number of H characters
-
-    Returns:
-        int: minimum number of operations
-    """
     if n <= 1:
         return 0
 
