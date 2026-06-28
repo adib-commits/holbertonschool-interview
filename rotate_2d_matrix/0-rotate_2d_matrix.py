8ae4f5e114b0e503c91b1f9d0b810bb90c874560
adib-commits <hdadib442@gmail.com>
2026-06-28 23:04:50 +0000

    rotates a square matrix 90° to the right without creating a new matrix

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for rotating a 2D matrix.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates an n x n matrix 90 degrees clockwise.
+
+    The rotation is done in-place, meaning the original
+    matrix is modified directly.
+    """
+
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = (
+                matrix[j][i],
+                matrix[i][j]
+            )
+
+    for row in matrix:
+        row.reverse()
